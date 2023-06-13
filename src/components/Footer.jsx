15cee4699ddd4458df6e6cb5fc8da530606b05d5
Arnold Rodriguez <arnolddavidrd@gmail.com>
2023-06-13 18:35:28 -0500

    footer como componente

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="container border-t py-5 text-center">
+      <p>Todos los derechos reservados &copy; FlashFood.</p>
+    </footer>
+  );
+};
+
+export default Footer;
