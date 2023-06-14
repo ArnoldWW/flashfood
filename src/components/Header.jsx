3871ad6d5614f85630f9027d7743430c3f5a0fc1
Arnold Rodriguez <arnolddavidrd@gmail.com>
2023-06-13 21:35:25 -0500

    imports innecesarios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import CartContext from "../context/CartContext";
 
 const Header = () => {
   const { cart } = useContext(CartContext);
-  console.log(cart.length);
+
   return (
     <header className="w-full sticky top-0 z-10 bg-white">
       <div className="flex flex-col justify-center items-center container py-5 gap-3 border-b">
@@ -14,7 +14,6 @@ const Header = () => {
 
         <nav className="flex justify-center gap-5 w-full">
           <Link to="/">Inicio</Link>
-
           <Link to="/menu">Menu</Link>
           <Link to="/cart" className="relative">
             Carrito <span className="font-bold">({cart.length})</span>
