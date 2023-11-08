88a4a91ff941605a2860d1805236fbc40c62338d
Arnold Rodriguez <arnolddavidrd@gmail.com>
2023-11-08 10:02:54 -0500

    mejoras en el header para la vista movil

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
       <div className="flex flex-col justify-center items-center container py-5 gap-3 border-b">
         <Logo />
 
-        <nav className="flex justify-between items-center w-full">
+        <nav className="flex flex-col md:flex-row md:justify-between items-center gap-5 w-full">
           <div className="flex gap-5 flex-1">
             <MyNavLink to="/">Inicio</MyNavLink>
             <MyNavLink to="/menu">Menú</MyNavLink>
@@ -26,13 +26,13 @@ const Header = () => {
 
           <div className="flex justify-between gap-2 items-center">
             {userData ? (
-              <>
+              <div className="flex justify-between gap-2 items-center">
                 <img src={userData.photoURL} className="w-5 rounded-full" />
                 <p>{userData.displayName} / </p>
                 <button className="btn w-auto" onClick={logOut}>
                   cerrar sesion
                 </button>
-              </>
+              </div>
             ) : (
               <button
                 className="btn flex gap-2 justify-center items-center"
