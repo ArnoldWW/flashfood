37da5c28345214c118e8b2b9ef905ed775588f7c
Arnold Rodriguez <arnolddavidrd@gmail.com>
2023-11-30 20:09:41 -0500

    cambios en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
 const Footer = () => {
+  const links = [
+    {
+      name: "Buger King",
+      url: "https://www.bk.com.co/"
+    },
+    {
+      name: "Little Caesars",
+      url: "https://co.littlecaesars.com/es-co/"
+    }
+  ];
+
   return (
-    <footer className="container border-t py-5 text-center">
-      <p>Todos los derechos reservados &copy; FlashFood.</p>
+    <footer className="container border-t py-5 text-left">
+      <p>&copy; FlashFood.</p>
+      <p>Imagenes tomadas de:</p>
+      <ul className="list-disc ml-5">
+        {links.map((link) => (
+          <li>
+            <a
+              href={link.url}
+              className="link"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              {link.name}
+            </a>
+          </li>
+        ))}
+      </ul>
     </footer>
   );
 };
